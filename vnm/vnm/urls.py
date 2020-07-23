25ad2828c118a296d81411f9d6a897d72ea841ff
Hao Wu <haowu0802@gmail.com>
2020-07-24 01:49:21 +0800

    add image panel

--- vnm/vnm/urls.py
+++ vnm/vnm/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from main import views
 
@@ -26,3 +28,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
