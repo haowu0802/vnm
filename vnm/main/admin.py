3045f0713d01d82c2f0eba89635df00633671462
Hao Wu <haowu0802@gmail.com>
2020-07-25 17:07:07 +0800

    add tags to image

--- vnm/main/admin.py
+++ vnm/main/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
-from .models import Novel, Actor, Matcher, Image
+from .models import Novel, Actor, Matcher, Image, Tag
+
+class TagListFilter(admin.SimpleListFilter):
+    
+    title = 'Has tag'
+
+    parameter_name = 'has_tag'
+
+    def lookups(self, request, model_admin):
+
+        return (
+            ('yes', 'Yes'),
+            ('no',  'No'),
+        )
+
+    def queryset(self, request, queryset):
+
+        if self.value() == 'yes':
+            return queryset.filter(tags__isnull=False)
+
+        if self.value() == 'no':
+            return queryset.filter(tags__isnull=True)
 
 class MatcherInline(admin.StackedInline):
     model = Matcher
@@ -20,7 +41,21 @@ class NovelAdmin(admin.ModelAdmin):
 class ActorAdmin(admin.ModelAdmin):
     list_display = ('name', )
 
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image_tag', 'actor', 'get_tags', 'file')
+    readonly_fields = ['image_tag']
+
+    list_filter = [TagListFilter,]
+
+
+    def get_tags(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    get_tags.short_description = 'Tags'
+
+
 # Register your models here.
 admin.site.register(Novel, NovelAdmin)
 admin.site.register(Actor, ActorAdmin)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Tag)
--- vnm/main/models.py
+++ vnm/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import mark_safe
 
 class Novel(models.Model):
     title = models.CharField(max_length=256, blank=True, null=True)
@@ -17,11 +18,17 @@ class Actor(models.Model):
 
 class Image(models.Model):
     title = models.CharField(max_length=256, blank=True, null=True)
-    file = models.FileField(upload_to='images/%Y/%m/%d')
+    file = models.ImageField(upload_to='images', blank=True)
     actor = models.ForeignKey(Actor, related_name='images', on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', related_name='tags', blank=True)
 
     def __str__(self):
         return self.file.url
+
+    def image_tag(self):
+            return mark_safe('<img src="/media/%s" width="100" height="100" />' % (self.file))
+
+    image_tag.short_description = 'Image'
 
 
 class Matcher(models.Model):
@@ -32,3 +39,10 @@ class Matcher(models.Model):
 
     def __str__(self):
         return self.match
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256)
+    weight = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
